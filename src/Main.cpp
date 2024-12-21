cba90b5712a78e1f07ed9c27848b9fa41335f4d7
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2024-12-21 15:48:41 +0100

    Add ASCII startup banner

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,22 @@
 #include <crow_all.h>
+#include <iostream>
+
+const char *banner = "  __  __       _       _               \n"
+                     " |  \\/  |     | |     | |              \n"
+                     " | \\  / | __ _| |_ __ | |__   __ _ ___ \n"
+                     " | |\\/| |/ _` | | '_ \\| '_ \\ / _` / __|\n"
+                     " | |  | | (_| | | |_) | | | | (_| \\__ \\\n"
+                     " |_|  |_|\\__,_|_| .__/|_| |_|\\__,_|___/\n"
+                     "                | |                    \n"
+                     "                |_|                    \n";
 
 int main()
 {
         crow::SimpleApp app;
 
-        CROW_ROUTE(app, "/")([](){
+        std::cout << banner << std::endl;
+
+        CROW_ROUTE(app, "/")([]() {
                 return "Hello world";
         });
 
