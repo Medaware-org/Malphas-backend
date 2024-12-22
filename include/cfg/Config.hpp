b3d51b94b35158d9d0bb13c6bb0db0fd7e0f5b34
RastiPatak <pat210600@spengergasse.at>
2024-12-22 15:59:39 +0100

    Add fopen_s to winwos platforms

--- include/cfg/Config.hpp
+++ include/cfg/Config.hpp
@@ -5,47 +5,50 @@
 #include <functional>
 #include <map>
 
-struct cfg_prop {
-        std::string key;
-        std::string value;
-        std::string section;
 
-        bool operator==(const char *path) const;
+struct cfg_prop
+{
+	std::string key;
+	std::string value;
+	std::string section;
+
+	bool operator==(const char* path) const;
 };
 
-class Config {
-    private:
-        std::vector<cfg_prop> props;
-        const char *path;
-        char *_buffer;
-        unsigned int buff_len;
-        bool parsed;
-    private:
-        std::unordered_map<std::string, std::function<bool(std::string)>> required;
-    public:
-        Config(const char *path);
+class Config
+{
+private:
+	std::vector<cfg_prop> props;
+	const char* path;
+	char* _buffer;
+	unsigned int buff_len;
+	bool parsed;
+private:
+	std::unordered_map<std::string, std::function<bool(std::string)>> required;
+public:
+	Config(const char* path);
 
-        ~Config();
+	~Config();
 
-        void add_required(std::string str);
-        void add_required(std::string str, std::function<bool(std::string)> validation);
+	void add_required(std::string str);
+	void add_required(std::string str, std::function<bool(std::string)> validation);
 
-        bool parse();
+	bool parse();
 
-        bool validate();
+	bool validate();
 
-        std::string operator[](const char *path);
+	std::string operator[](const char* path);
 
-    private:
-        bool read_file();
+private:
+	bool read_file();
 };
 
 //
 // Parser Utilities
 //
 
-int skip_spaces(char **buff);
+int skip_spaces(char** buff);
 
-int parse_identifier(char **buff, std::string &dst);
+int parse_identifier(char** buff, std::string& dst);
 
-int parse_value(char **buff, std::string &dst);
+int parse_value(char** buff, std::string& dst);
