53a1cc27555292cd3671e737ec2a88e6771bc057
Piotr K. Wyrwas <pw.piotr.wyrwas@gmail.com>
2025-01-05 21:48:49 +0100

    Add a basic string sanitizer to make the functions less prone to SQL injections

--- include/dao_preamble.h
+++ include/dao_preamble.h
@@ -8,25 +8,52 @@
 
 #define IFNNULL(n, _else) (PQgetisnull(result, tuple, n) ? std::nullopt : std::optional(_else))
 
+inline std::string sql_sanitize(std::string str)
+{
+        std::string sanitized;
+        for (const auto &c: str) {
+                switch (c) {
+                        case '\\':
+                                sanitized += "\\\\";
+                                break;
+                        case '\'':
+                                sanitized += "''";
+                                break;
+                        case '\"':
+                                sanitized += "\\\"";
+                                break;
+                        default:
+                                sanitized += c;
+                }
+        }
+        return sanitized;
+}
+
 [[nodiscard]] inline bool cast_bool(std::string &&str) { return (str == "true"); }
 
 template<typename T>
 [[nodiscard]] typename std::enable_if<!std::is_same<T, std::string>::value && !std::is_same<T, bool>::value,
-        std::string>::type xto_string(T arg)
+        std::string>::type xto_string_impl(T arg)
 {
         return std::to_string(arg);
 }
 
 template<typename T>
-[[nodiscard]] typename std::enable_if<std::is_same<T, std::string>::value, std::string>::type xto_string(T arg)
+[[nodiscard]] typename std::enable_if<std::is_same<T, std::string>::value, std::string>::type xto_string_impl(T arg)
 {
         return arg;
 }
 
 template<typename T>
-[[nodiscard]] typename std::enable_if<std::is_same<T, bool>::value, std::string>::type xto_string(T arg)
+[[nodiscard]] typename std::enable_if<std::is_same<T, bool>::value, std::string>::type xto_string_impl(T arg)
 {
         return (arg ? "true" : "false");
+}
+
+template<typename T>
+[[nodiscard]] std::string xto_string(T arg)
+{
+        return sql_sanitize(xto_string_impl(arg));
 }
 
 //
@@ -43,6 +70,7 @@ inline bool finalize_op(PGresult *res)
 
 inline PGresult *dao_query(Database &db, std::string query, ExecStatusType assert_status)
 {
+        CROW_LOG_INFO << "Query: " << query;
         ExecStatusType status;
         PGresult *res = db.query(query, &status);
         if (status != assert_status) {
